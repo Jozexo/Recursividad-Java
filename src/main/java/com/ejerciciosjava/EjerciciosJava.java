2d51f9067cf979dba19e945b5daf22de49c5e2f6
José Hernández <106791212+Jozexo@users.noreply.github.com>
2024-07-16 23:35:30 -0400

    Update, add ejercicio 2

--- src/main/java/com/ejerciciosjava/EjerciciosJava.java
+++ src/main/java/com/ejerciciosjava/EjerciciosJava.java
@@ -23,6 +23,32 @@ public class EjerciciosJava {
     
     
     
+    /* 2. Con un TAD lista circular precargado con nodos que contengan como valor 
+    almacenado una palabra , desarrolle una función que permita concatenar todos 
+    los valores y agregar un espacio de por medio(" "), retornando esa cadena de 
+    texto resultante al final de la operación.*/
+    public static class Nodo {
+        String valor;
+        Nodo siguiente;
+
+        public Nodo(String valor){
+            this.valor = valor;
+            this.siguiente = null;
+        }
+    }
+    public static String concatenarValores(Nodo cabeza){
+        return concatenarValoresR(cabeza, "", cabeza);
+    }
+    private static String concatenarValoresR(Nodo actual, String resultado, Nodo cabeza){
+        if (actual == cabeza && resultado.length() > 0){
+            return resultado.trim();
+    } else {
+        return concatenarValoresR(actual.siguiente, resultado + actual.valor + " ", cabeza);
+    }
+}
+    
+    
+    
    
     
     
@@ -36,6 +62,19 @@ public class EjerciciosJava {
         System.out.println("==========================================");
         System.out.println("1 --> SUMA RECURSIVA DE UN ARREGLO -->: " + resultadoSuma);
         System.out.println("==========================================");
+        
+        // 2 --> Concatenación de valores en una lista circular
+        Nodo nodo1 = new Nodo("CAFE NO DISPONIBLE.");
+        Nodo nodo2 = new Nodo("INSTALANDO JAVA...");
+        Nodo nodo3 = new Nodo("INSTALACION COMPLETA. VUELVA A INTENTARLO!");
+        nodo1.siguiente = nodo2;
+        nodo2.siguiente = nodo3;
+        nodo3.siguiente = nodo1;
+        
+        String resultadoConcatenacion = concatenarValores(nodo1);
+        System.out.println("==========================================================================================================================================");
+        System.out.println("2 --> CONCATENACION DE VALORES EN LA LISTA CIRCULAR -->: " + resultadoConcatenacion);
+        System.out.println("==========================================================================================================================================");
         
         
     }
